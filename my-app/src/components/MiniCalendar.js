d4e3f4b269321e80d987b40a30be8eb230d49890
John Liu <john@goliu.com>
2022-11-13 14:54:56 -0500

    added pages folder, MiniCalendar component

---
+++ my-app/src/components/MiniCalendar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const MiniCalendar = () => {
+  return (
+    <div>
+      MiniCalendar
+    </div>
+  )
+}
+
+export default MiniCalendar
---
+++ my-app/src/pages/TestMiniCalendar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MiniCalendar from "./components/MiniCalendar"
+
+
+const TestMiniCalendar = () => {
+  return (
+    <div>
+      <h1>
+        TestMiniCalendar
+      </h1>
+      < MiniCalendar />
+    </div>
+  )
+}
+
+export default TestMiniCalendar
