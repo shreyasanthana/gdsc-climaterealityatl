728a6aee45dd62d3d179a6aa10af2db8fde665a4
EBruda <elizabethb10124@gmail.com>
2022-11-25 13:56:45 -0500

    updated donate page component to display on react

--- my-app/src/DonatePage.js
+++ my-app/src/DonatePage.js
@@ -1,36 +1,34 @@
-import { 
-    Box,
-    Image
-    
-} from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 
 const DonatePage = () => {
   return (
-    <Box id="donate-layout" style="display: flex; flex-direction: column">
-      <Box id="row-1-donate" style="display: flex; flex-direction: row">
-        <Box style="display: flex; flex-direction: column">
-          <h2>Call to Action</h2>
-          <p>Fill in info</p>
+    <Box>
+      <Box style={{ display: 'flex', flexDirection: 'column' }}>
+        <Box style={{ display: 'flex', flexDirection: 'row' }}>
+          <Box style={{ display: 'flex', flexDirection: 'column' }}>
+            <h2>Call to Action</h2>
+            <p>Fill in info</p>
+          </Box>
+          <Box boxSize="sm">
+            <Image
+              src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
+              alt="Climate Reality Atl"
+              style={{ height: '10em', width: '10em' }}
+            />
+          </Box>
         </Box>
-        <Box boxSize="sm">
-          <Image
-            src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
-            alt="Climate Reality Atl"
-            style="height: 10em; width: 10em"
-          />
-        </Box>
-      </Box>
-      <Box id="row-2-donate">
         <Box>
-          <h2>Make Donation</h2>
-          <input />
-        </Box>
-        <Box boxSize="sm">
-          <Image
-            src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
-            alt="Climate Reality Atl"
-            style="height: 10em; width: 10em"
-          />
+          <Box>
+            <h2>Make Donation</h2>
+            <input />
+          </Box>
+          <Box boxSize="sm">
+            <Image
+              src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
+              alt="Climate Reality Atl"
+              style={{ height: '10em', width: '10em' }}
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
