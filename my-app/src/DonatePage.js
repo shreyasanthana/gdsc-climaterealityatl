3523ca80278e58e6a11a7c502b8da2005b00841e
EBruda <elizabethb10124@gmail.com>
2022-11-25 13:40:28 -0500

    added div of donate page html inside component of donatePage.js

--- my-app/src/DonatePage.js
+++ my-app/src/DonatePage.js
@@ -1,10 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
 
 const DonatePage = () => {
-    return (
-        <Box>
-            <h1>Donate</h1>
+  return (
+    <div id="donate-layout" style="display: flex; flex-direction: column">
+      <div id="row-1-donate" style="display: flex; flex-direction: row">
+        <div style="display: flex; flex-direction: column">
+          <h2>Call to Action</h2>
+          <p>Fill in info</p>
+        </div>
+        <Box boxSize="sm">
+          <image
+            src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
+            alt="Climate Reality Atl"
+            style="height: 10em; width: 10em"
+          />
         </Box>
-    );
-}
+      </div>
+      <div id="row-2-donate">
+        <div>
+          <h2>Make Donation</h2>
+          <input />
+        </div>
+        <Box boxSize="sm">
+          <image
+            src="https://www.greenbiz.com/sites/default/files/images/articles/featured/24hoursofhope.jpg"
+            alt="Climate Reality Atl"
+            style="height: 10em; width: 10em"
+          />
+        </Box>
+      </div>
+    </div>
+  );
+};
 export default DonatePage;
