768efa92caddf7a8055dc324271fde081ba75fd6
John Liu <john@goliu.com>
2022-11-30 04:44:50 -0500

    added skeleton files

---
+++ my-app/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, Heading, Box, Image } from "@chakra-ui/react";
+
+const About = () => {
+  return (
+    <div>
+      <Text color="black">
+        About
+      </Text>
+      <Box width="1200px" height="800px" bgGradient='linear(to-r, red.500, green)' />
+    </div>
+  )
+}
+
+export default About;
---
+++ my-app/src/pages/GetInvolved.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const GetInvolved = () => {
+  return (
+    <div>GetInvolved</div>
+  )
+}
+
+export default GetInvolved;
