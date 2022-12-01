6c1ceca3cdf3b5348c51367661c398d76b647941
John Liu <john@goliu.com>
2022-12-01 00:42:14 -0500

    added skeleton file

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, Box } from "@chakra-ui/react";
+
+
+const Footer = () => {
+  return (
+    <Box>
+      <Text ml="10px" mt="5px" fontSize="xl" color="white">
+        Footer
+      </Text>
+    </Box>
+  )
+}
+
+export default Footer;
