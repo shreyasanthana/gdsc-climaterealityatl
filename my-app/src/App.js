e8a2574f85c270632d39ef2a72d99fe68b27d60f
tylerttam <tylertam571@gmail.com>
2022-12-02 10:11:05 -0500

    form input connected with google forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import Form from "./Forms";
 import React from 'react';
 import {
   ChakraProvider,
@@ -19,71 +20,78 @@ import { Textarea } from '@chakra-ui/react';
 import {NumberInput} from '@chakra-ui/react'
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+    // <ChakraProvider theme={theme}>
+    //   <Box textAlign="center" fontSize="xl">
         
-        <Grid minH="100vh" p={3} templateColumns='repeat(5, 1fr)'>
-        <GridItem colSpan={2}>
-            <VStack spacing={5}>
-            <Text fontSize="l"> Have a suggestion? Leave it here! </Text>
-            <Box textAlign="left" fontsize="m" w='100%'>
-              <FormControl isRequired>
-                <FormLabel> Name: </FormLabel>
-                <Input placeholder="First and Last Name" />
-              </FormControl>
-            </Box>
+    //     <Grid minH="100vh" p={3} templateColumns='repeat(5, 1fr)'>
+    //     <GridItem colSpan={2}>
+    //         <VStack spacing={5}>
+    //         <Text fontSize="l"> Have a suggestion? Leave it here! </Text>
+    //         <Box textAlign="left" fontsize="m" w='100%'>
+    //           <FormControl isRequired>
+    //             <FormLabel> Name: </FormLabel>
+    //             <Input placeholder="First and Last Name" />
+    //           </FormControl>
+    //         </Box>
 
-            <Box textAlign="left" fontsize="m" w='100%'>
-            <FormControl isRequired>
-                <FormLabel> Email Address </FormLabel>
-                <Input type='email' placeholder='username@website.com'/>
-              </FormControl>
-            </Box>
+    //         <Box textAlign="left" fontsize="m" w='100%'>
+    //         <FormControl isRequired>
+    //             <FormLabel> Email Address </FormLabel>
+    //             <Input type='email' placeholder='username@website.com'/>
+    //           </FormControl>
+    //         </Box>
 
-            <Box textAlign="left" fontsize="m" w='100%'>
-            <FormControl>
-                <FormLabel> Age </FormLabel>
-                <Input type='number' placeholder='18'/>
-              </FormControl>
-            </Box>
+    //         <Box textAlign="left" fontsize="m" w='100%'>
+    //         <FormControl>
+    //             <FormLabel> Age </FormLabel>
+    //             <Input type='number' placeholder='18'/>
+    //           </FormControl>
+    //         </Box>
 
-            <Box textAlign="left" fontsize="m" w='100%'>
-            <FormControl>
-                <FormLabel> Phone Number: </FormLabel>
-                <Input type='tel' placeholder='000-000-0000'/>
-              </FormControl>
-            </Box>
+    //         <Box textAlign="left" fontsize="m" w='100%'>
+    //         <FormControl>
+    //             <FormLabel> Phone Number: </FormLabel>
+    //             <Input type='tel' placeholder='000-000-0000'/>
+    //           </FormControl>
+    //         </Box>
 
-            <Box textAlign="left" fontsize="m" w='100%'>
-            <Text fontsize="md"> Suggestion: </Text>
-            <Textarea placeholder="Write your suggestion here!" />
-            </Box>
+    //         <Box textAlign="left" fontsize="m" w='100%'>
+    //         <Text fontsize="md"> Suggestion: </Text>
+    //         <Textarea placeholder="Write your suggestion here!" />
+    //         </Box>
 
-            </VStack>
-          </GridItem>
+    //         </VStack>
+    //       </GridItem>
 
-          <GridItem colSpan={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-          </GridItem>
-        </Grid>
-      </Box>  
+    //       <GridItem colSpan={3}>
+    //       <ColorModeSwitcher justifySelf="flex-end" />
+    //       <VStack spacing={8}>
+    //         <Logo h="40vmin" pointerEvents="none" />
+    //         <Text>
+    //           Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+    //         </Text>
+    //         <Link
+    //           color="teal.500"
+    //           href="https://chakra-ui.com"
+    //           fontSize="2xl"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           Learn Chakra
+    //         </Link>
+    //       </VStack>
+    //       </GridItem>
+    //     </Grid>
+    //   </Box>  
       
-    </ChakraProvider>
+    // </ChakraProvider>
+    <div className="App">
+    <div className="formWrapper">
+      <div className="contactForm">
+        <Form />
+      </div>
+    </div>
+  </div>
   );
 }
 
