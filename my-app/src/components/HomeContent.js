ac74973b4e0f26183a356019b2357c1de292e3a9
John Liu <john@goliu.com>
2022-12-02 11:10:37 -0500

    added modularization

--- my-app/src/components/HomeContent.js
+++ my-app/src/components/HomeContent.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Box, Flex, Heading, Text, Image, Spacer } from "@chakra-ui/react";
+import HomeContentText from "./HomeContentText";
+import HomeCalendar from "./HomeCalendar";
+
 
 // Main content below Hero image area
 const HomeContent = () => {
   return (
-    <Box
+    <Flex
       mt="40px"
-      ml="20px"
+      mx="20px"
       mb="120px"  // for proper footer placement
+      gap="80px" // spacing between text and calendar component
     >
-      <Text fontSize="3xl">Some text below Hero image</Text>
-      <Text fontSize="3xl">Some more text below Hero image</Text>
-      <Text fontSize="3xl">Even more text below Hero image</Text>
-    </Box>
+
+      {/* box containing text narrative */}
+      <Box
+        width="70%"
+        border="1px"  // TODO: remove
+      >
+        <HomeContentText />
+      </Box>
+
+      {/* box containing embedded responsive mini Calendar */}
+      <Box
+        width="30%"
+        border="2px"  // TODO: possibly remove
+      >
+        <HomeCalendar />
+      </Box>
+    </Flex>
   )
 }
 
---
+++ my-app/src/components/HomeContentText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Flex, Heading, Text, Image, Spacer } from "@chakra-ui/react";
+
+
+const HomeContentText = () => {
+  return (
+    <Box
+      p="20px"
+    >
+      <Text fontSize="3xl">Some text below Hero image</Text>
+      <Text fontSize="3xl">Some more text below Hero image</Text>
+      <Text fontSize="3xl">Even more text below Hero image</Text>
+    </Box>
+  )
+}
+
+export default HomeContentText;
