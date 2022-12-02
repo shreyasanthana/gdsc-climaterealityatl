55d782d6d21f03e9139575bd426d034c3e6a6978
vidushiMaheshwari <vidushi.maheshwari04@gmail.com>
2022-12-02 08:36:15 -0500

    calendar iframe styling

--- my-app/src/pages/EventsPage.js
+++ my-app/src/pages/EventsPage.js
@@ -1,12 +1,23 @@
-import { Box } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import "./cal.css";
 
 function EventPage() {
+
     return (
-        <Box>
-            <h1>Calendar</h1>
-            <iframe src="https://calendar.google.com/calendar/embed?src=c7b707e6c9ed9e760a3805267c524c6d875a85297dab6a32a641d07f6ad36fc4%40group.calendar.google.com&ctz=America%2FNew_York" title="Upcoming Events" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
-        </Box>
-    )
+      <Box>
+        <div className="calendar">
+          <h1 className="cal-head">Calendar Events</h1>
+          <iframe
+            title="Upcoming Events"
+            src="https://calendar.google.com/calendar/embed?src=c7b707e6c9ed9e760a3805267c524c6d875a85297dab6a32a641d07f6ad36fc4%40group.calendar.google.com&ctz=America%2FNew_York"
+            width="600"
+            height="800"
+            frameBorder="0"
+            scrolling="no"
+          ></iframe>
+        </div>
+      </Box>
+    );
 }
 
 export default EventPage
