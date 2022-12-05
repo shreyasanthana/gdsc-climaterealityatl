0dce568ec8a358aadc7941b50badd3af5039737a
Vincent Horvath <vhorvath3@gatech.edu>
2022-12-05 16:58:14 -0500

    add react basics

---
+++ my-app/src/components/ProfileCard.js
@@ -0,0 +1,7 @@
+export default function ProfileCard({name, role}) {
+    return (
+        <div>
+            <h1>{name} : {role}</h1>
+        </div>
+    )
+}
---
+++ my-app/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import ProfileCard from "../components/ProfileCard";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <ProfileCard name="Vince" role="Lead"></ProfileCard>
+        </div>
+    )
+}
