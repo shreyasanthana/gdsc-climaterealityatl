a8d4f62aa18bbbda0424aa46c78b9f76977b5804
John Liu <john@goliu.com>
2022-12-08 16:14:31 -0500

    added hero text

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -22,15 +22,12 @@ const Hero = () => {
           objectFit="cover"
         />
         <Flex
-          flexDirection="column"
+          width={{ base: "full", lg: "70%" }}
           position="absolute"
-          top="390px"
-          left="150px"
-          gap="10px"
+          top="400px"
+          left="50px"
         >
-          <Text color="yellow" fontSize="4xl" fontWeight="bold">Some text inside Hero</Text>
-          <Text color="yellow" fontSize="4xl" fontWeight="bold">Some more text inside Hero</Text>
-          <Text color="yellow" fontSize="4xl" fontWeight="bold">Even more text inside Hero</Text>
+          <Text color="white" fontSize={{ base: "2xl", md: "3xl", xl: "4xl" }} fontWeight="bold" lineHeight="1.0" pr="100px">As a community chapter of The Climate Reality Project founded by former Vice President Al Gore, we play a crucial role in promoting clean, efficient, and sustainable energy consumption and transportation practices right here in Atlanta. </Text>
         </Flex>
       </Box>
     </Box>
