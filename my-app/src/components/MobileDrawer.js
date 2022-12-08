b524df0120d6483cfdf6eb97ff680147646c690f
John Liu <john@goliu.com>
2022-12-08 15:18:32 -0500

    added feat: responsive drawer size

--- my-app/src/components/MobileDrawer.js
+++ my-app/src/components/MobileDrawer.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { useDisclosure, Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay,
-  DrawerContent, DrawerCloseButton, Button, Flex, Text } from "@chakra-ui/react";
+  DrawerContent, DrawerCloseButton, Button, Flex, Text, Spacer, Box } from "@chakra-ui/react";
 import { ImMenu, ImCross } from "react-icons/im";
 import MenuLinks from "./MenuLinks";
 
 const MobileDrawer = ({ isOpen, onOpen, onClose }) => {
-  // const {isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Flex>
       <Button variant="unstyled" onClick={onOpen}>
         <ImMenu size="36px" color="white" />
-        <Drawer placement="right" onClose={onClose} isOpen={isOpen} display={{ base: "flex", lg: "none" }}>
+        <Drawer size={{ base: "full", md: "xs" }} placement="right" onClose={onClose} isOpen={isOpen} >
           <DrawerOverlay />
           <DrawerContent bg="black">
             <DrawerCloseButton size="lg" color="white"/>
-            <DrawerHeader justify="right">
-              <Flex display="flex" height="69px" alignItems="center" justifyContent="right">
-                {/* <Button variant="unstyled" onClick={onClose}>
-                  <ImCross size="26px" color="white" />
-                </Button> */}
-              </Flex>
+            <DrawerHeader>
+              <Box height="69px" />
             </DrawerHeader>
             <DrawerBody color="white">
               <MenuLinks flexDirProp="column" />
