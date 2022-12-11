f71a20a488f2a1a27a832fd35ed52f64c3b9255f
Vincent Horvath <vhorvath3@gatech.edu>
2022-12-11 17:05:29 -0500

    update style slightly

--- my-app/src/components/ProfileCard.js
+++ my-app/src/components/ProfileCard.js
@@ -1,24 +1,15 @@
-import { Badge, Image, Button, Center } from "@chakra-ui/react"
+import { Badge, Image, Center, Box } from "@chakra-ui/react"
 
-import { MdCall, MdFormatAlignCenter } from "react-icons/md"
-
-
-
-export default function ProfileCard({name, role, email, phone_number, linkedin, instagram}) {
+export default function ProfileCard({ name, role, email, phone_number, linkedin, instagram }) {
     return (
-        <div style={{textAlign: "center", paddingTop : "2%"}}>
+        <Box bg='#8dc63f' height='350px' style={{ textAlign: "center", paddingTop: "2%" }} borderRadius={25}>
             <Center><Image borderRadius='full' boxSize='150px' src='https://bit.ly/dan-abramov' /></Center>
-            <h1><b> Name:</b>{name}</h1>
-            <h1><b> Role:</b> <Badge borderRadius='full' px='2' colorScheme='teal'>{role}</Badge> </h1>
-            <h1><b> Email:</b>{email}</h1>
-            <h1><b> Phone Number:</b>{phone_number}</h1>
-            <h1><b> LinkedIn:</b>{linkedin}</h1>
-            <h1><b> Instagram:</b>{instagram}</h1>
-            
-            <Button rightIcon={<MdCall />} colorScheme='blue' variant='outline'>
-                Call {name}
-            </Button>
-        </div>
-
+            <h1><b> Name: </b>{name}</h1>
+            <h1><b> Role: </b> <Badge borderRadius='full' px='2' colorScheme='blackAlpha'>{role}</Badge> </h1>
+            <h1><b> Email: </b>{email}</h1>
+            <h1><b> Phone Number: </b>{phone_number}</h1>
+            <h1><b> LinkedIn: </b>{linkedin}</h1>
+            <h1><b> Instagram: </b>{instagram}</h1>
+        </Box>
     )
 }
--- my-app/src/pages/AboutPage.js
+++ my-app/src/pages/AboutPage.js
@@ -1,31 +1,18 @@
 import ProfileCard from "../components/ProfileCard";
 import '../components/ProfileCard.css';
-import { Box, SimpleGrid } from "@chakra-ui/react"
+import { SimpleGrid } from "@chakra-ui/react"
 
 export default function AboutPage() {
     return (
         <div >
-            <h1 style={{paddingTop: "2%", paddingBottom: "2.5%", fontSize: '30px', textAlign: "center"}}>This is the About Page Header</h1>
-
-            <SimpleGrid columns={3} spacing={10} pr = "15px" pl = "15px">
-                <Box bg='#BEE3F8' height='350px'>
-                    <ProfileCard name="Vince" role="Lead" email = "test@gmail.com" phone_number = "(472) 472-3883" linkedin = "www.linkedin.com/test/" instagram = "@test.gatech" />
-                </Box>
-                <Box bg='#BEE3F8' height='350px'>
-                    <ProfileCard name="Maddy" role="Developer" email = "maddy@gmail.com" phone_number = "(872) 826-2245" linkedin = "https://www.linkedin.com/in/maddy/" instagram = "@test.gatech"/>
-                </Box>
-                <Box bg='#BEE3F8' height='350px'>
-                    <ProfileCard name="John" role="UI/UX" email = "john@gmail.com" phone_number = "(384) 472-3775" linkedin = "https://www.linkedin.com/in/john/" instagram = "@john.gatech"/>
-                </Box>
-                <Box bg='#BEE3F8' height='350px'>
-                    <ProfileCard name="Maddy" role="Developer" email = "maddy@gmail.com" phone_number = "(872) 826-2245" linkedin = "https://www.linkedin.com/in/maddy/" instagram = "@maddy.gatech"/>
-                </Box>
-                <Box bg='#BEE3F8' height='350px'>
-                    <ProfileCard name="Dani" role="Manager" email = "dani@gmail.com" phone_number = "(474) 993-9273" linkedin = "https://www.linkedin.com/in/dani/" instagram = "@dani.gatech"/>
-                </Box>
-                <Box bg='#BEE3F8' height='350px'></Box>
+            <h1 style={{ paddingTop: "2%", paddingBottom: "2.5%", fontSize: '30px', textAlign: "center" }}>This is the About Page Header</h1>
+            <SimpleGrid columns={[1, 1, 2, 3]} spacing={10} pr="15px" pl="15px">
+                <ProfileCard name="Vince" role="Lead" email="test@gmail.com" phone_number="(472) 472-3883" linkedin="www.linkedin.com/test/" instagram="@test.gatech" />
+                <ProfileCard name="Maddy" role="Developer" email="maddy@gmail.com" phone_number="(872) 826-2245" linkedin="https://www.linkedin.com/in/maddy/" instagram="@test.gatech" />
+                <ProfileCard name="John" role="UI/UX" email="john@gmail.com" phone_number="(384) 472-3775" linkedin="https://www.linkedin.com/in/john/" instagram="@john.gatech" />
+                <ProfileCard name="Maddy" role="Developer" email="maddy@gmail.com" phone_number="(872) 826-2245" linkedin="https://www.linkedin.com/in/maddy/" instagram="@maddy.gatech" />
+                <ProfileCard name="Dani" role="Manager" email="dani@gmail.com" phone_number="(474) 993-9273" linkedin="https://www.linkedin.com/in/dani/" instagram="@dani.gatech" />
             </SimpleGrid>
-
         </div>
     )
 }
