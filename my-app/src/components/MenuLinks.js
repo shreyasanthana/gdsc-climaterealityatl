c9a0d5d0e7af03580e494f679283a867c1f0ac9c
John Liu <john@goliu.com>
2022-12-19 15:01:21 -0500

    fixing donate button link

--- my-app/src/components/MenuLinks.js
+++ my-app/src/components/MenuLinks.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { HStack, Link, Flex, Text, Heading, Button,
-         Box } from "@chakra-ui/react";
+import { HStack, Link, Flex, Text, Heading, Button, Box } from "@chakra-ui/react";
+import { HiOutlineExternalLink } from "react-icons/hi";
+const DonateLink = "https://www.climaterealityproject.org/donate/chapters-support-climate-reality-project-atlanta-ga";
+
 
 const MenuLinks = ({ flexDirProp }) => {
   const mb = (flexDirProp === "column" ? 3 : 0);
@@ -20,20 +22,24 @@ const MenuLinks = ({ flexDirProp }) => {
         <Link mr={mr} mb={mb} fontSize="lg" letterSpacing="1px" _hover={{ color:"brandGreen" }} href="/events">Events</Link>
         <Link mr={mr} mb={mb} fontSize="lg" letterSpacing="1px" _hover={{ color:"brandGreen" }} href="/getinvolved">Get Involved</Link>
         <Link mr={mr} mb={mb} fontSize="lg" letterSpacing="1px" _hover={{ color:"brandGreen" }} href="/contact">Contact</Link>
-        <Button
-          mr={mr}
-          mt={mt}
-          variant="outline"
-          textTransform="uppercase"
-          border="2px"
-          fontSize="lg"
-          letterSpacing="1px"
-          _hover={{
-            background:"brandGreen",
-          }}
-        >
+        <Link href={DonateLink} target="_blank">
+          <Button
+            mr={mr}
+            mt={mt}
+            variant="outline"
+            textTransform="uppercase"
+            border="2px"
+            fontSize="lg"
+            letterSpacing="1px"
+            // bg="blue.700"
+            _hover={{
+              background:"brandGreen",
+            }}
+          >
             donate
+            {/* <HiOutlineExternalLink size="20px" ml="30px" /> */}
           </Button>
+        </Link>
       </Flex>
   )
 }
