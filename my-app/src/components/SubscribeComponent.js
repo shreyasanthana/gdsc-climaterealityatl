7674a0c24a1b17beaf580c808ff86930b27e84f5
John Liu <john@goliu.com>
2023-01-08 22:09:31 -0500

    changed text

--- my-app/src/components/SubscribeComponent.js
+++ my-app/src/components/SubscribeComponent.js
@@ -7,7 +7,7 @@ function SubscribeComponent() {
     <ChakraProvider theme={theme}>
       <Box fontSize="xl" align="center">
         <Grid minH="1vh" p={0}>
-        <Text fontSize={{ base: '24px', md: '30px', lg: '30px' }} fontWeight = 'bold'> Subscribe to our mailchimp here:</Text>
+        <Text fontSize={{ base: '24px', md: '30px', lg: '30px' }} fontWeight = 'bold'> Subscribe to our mailing list here:</Text>
           <VStack spacing={20}>
           <Mailchimp
                 action="https://website.us20.list-manage.com/subscribe/post?u=249e1c6faeb13ab1351388d1f&amp;id=b2c9d93035&amp;f_id=00044ee6f0"
